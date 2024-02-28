95a44c105e01d44f9b02d1017cdc7bef164ca9f8
ashish-kumar-patel <patelashishce@gmail.com>
2024-02-28 23:48:39 +0530

    responive the footer

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,105 +1,115 @@
 import React from "react";
-import { FaInstagram } from "react-icons/fa";
-import { FaFacebookF } from "react-icons/fa";
-import { FaLinkedin } from "react-icons/fa";
-import { FaYoutube } from "react-icons/fa";
+import { FaInstagram ,FaFacebookF, FaLinkedin ,FaYoutube } from "react-icons/fa";
 import FooterImage1 from "../assets/FooterImage1.png"
 import FooterImage2 from "../assets/FooterImage2.png"
 
+
 const Footer = () => {
 
+  const cities = ["Bengaluru", "Mumbai", "Surat ", "Gujarat ", "Rajastan"];
+  const exploreItems = [ "About Us", "Contact Us","Blogs", "Pricing", "Careers",];
+  const informationItems = ["Tracking", "Rate Calculator", "Media", "FAQs", "Glossary",];
+
   return (
-    <footer className="  bg-[#011F61] text-white   py-4">
-      <div className="container mx-auto  px-9 pb-7">
-        <div className="flex  justify-around   px-1  mr-20">
-          <div className="w-full md:w-1/3 text-center md:text-left">
-            <h1 className="  text-5xl font-bold text-black  ">Logo</h1>
-            <h5 className="  text-lg font-bold mb-3 mt-4">Our Locations</h5>
-            <ul className="list-unstyled  cursor-pointer   text-gray-200">
-              <li>Mumbai</li>
-              <li>Surat</li>
-              <li>Gurugram</li>
-              <li>Aurangabad</li>
-              <li>Bengaluru</li>
+    <footer className="  bg-[#011F61] text-white   py-4 pt-12">
+     <div className="container mx-auto  ">
+        <div className="grid grid-cols-2 min-w-96 md:grid-cols-5  lg:pl-10 lg:pr-28">
+          <div className="text-center md:text-left">
+            <h1 className="  text-5xl font-bold text-black  mb-5 ">Logo</h1>
+            <h5 className="text-lg font-bold mb-3">Our Locations</h5>
+            <ul className="list-none text-gray-200">
+              {cities.map((city, index) => (
+                <li key={index} className=" mb-1">
+                  {city}
+                </li>
+              ))}
             </ul>
           </div>
-          <div className="w-full md:w-1/3 text-center  md:text-left">
-            <h5 className="  text-lg font-bold mb-3">Explore</h5>
-            <ul className="list-unstyled   text-gray-200 cursor-pointer">
-              <li className=" mb-2">About Us</li>
-              <li className=" mb-2">Contact Us</li>
-              <li className=" mb-2">Blogs</li>
-              <li className=" mb-2">Pricing</li>
-              <li className=" mb-2">Careers</li>
+          <div className="text-center md:text-left">
+            <h5 className="text-lg font-bold mb-3">Explore</h5>
+            <ul className="list-none text-gray-200">
+              {exploreItems.map((item, index) => (
+                <li key={index} className="mb-3">
+                  {item}
+                </li>
+              ))}
             </ul>
           </div>
-          <div className="w-full md:w-1/3 text-center md:text-left">
-            <h5 className="  text-lg font-bold mb-3">Information</h5>
-            <ul className="list-unstyled  cursor-pointer  text-gray-200">
-              <li className=" mb-2">Tracking</li>
-              <li className=" mb-2">Rate Calculator</li>
-              <li className=" mb-2">Media</li>
-              <li className=" mb-2">FAQs</li>
-              <li className=" mb-2">Glossary</li>
+          <div className="text-center md:text-left">
+            <h5 className="text-lg font-bold mb-3">Information</h5>
+            <ul className="list-none text-gray-200">
+              {informationItems.map((item, index) => (
+                <li key={index} className="mb-3">
+                  {item}
+                </li>
+              ))}
             </ul>
           </div>
-          <div className="w-full md:w-1/3 text-center md:text-left">
-            <h5 className="  text-lg mb-3  font-bold">Connect with us</h5>
-            <ul className="list-unstyled   text-gray-200">
-              <li className=" mb-2 cursor-pointer">9999999999</li>
-              <li className=" mb-2  cursor-pointer">business@.com</li>
-            </ul>
-            <h5 className="  text-base mb-2 mt-8 font-bold">Follow Us On</h5>
-            <ul className="social-media list-unstyled  text-white mt-5 flex gap-2 text-xl">
-              <li>
-                <a href="#">
-                  <FaFacebookF />
-                </a>
-              </li>
-              <li>
-                <a href="#">
-                  <FaInstagram />
-                </a>
-              </li>
-              <li>
-                <a href="#">
-                  <FaLinkedin />
-                </a>
-              </li>
-              <li>
-                <a href="#">
-                  <FaYoutube />
-                </a>
-              </li>
-            </ul>
-          </div>
-          <div className="">
+          <div className="w-full  md:w-1/2  md:px-0 text-center ">
             <div>
-              <h4 className=" text-xl font-bold mb-8">Sign up to get offers</h4>
+              <h5 className="text-lg font-bold mb-3 ">Connect with us</h5>
+              <ul className="list-none text-gray-200">
+                <li className=" cursor-pointer">9999999999</li>
+                <li className="mb-2 cursor-pointer">business@.com</li>
+              </ul>
             </div>
-            <div className=" ">
-              <form >
-               <div className=" flex  border-slate-400 border-2   rounded-3xl  py-1 overflow-hidden  ">
-               <input type="email" placeholder="Email Address " className=" bg-transparent  pl-4 outline-none" />
-                <button type="submit" className=" bg-blue-800 hover:bg-blue-900  px-4 py-1  -my-1">Go</button>
-               </div>
+            <div className="  lg:mt-6  ">
+              <h5 className="text-base mb-2 font-bold">Follow Us On</h5>
+              <ul className="flex text-white text-xl gap-2  justify-center ">
+                <li>
+                  <a href="#" className="cursor-pointer">
+                    <FaFacebookF />
+                  </a>
+                </li>
+                <li>
+                  <a href="#" className="cursor-pointer">
+                    <FaInstagram />
+                  </a>
+                </li>
+                <li>
+                  <a href="#" className="cursor-pointer">
+                    <FaLinkedin />
+                  </a>
+                </li>
+                <li>
+                  <a href="#" className="cursor-pointer">
+                    <FaYoutube />
+                  </a>
+                </li>
+              </ul>
+            </div>
+          </div>
+
+          <div className="  col-span-2  sm:col-span-2   text-center   md:col-span-1  lg:col-span-1 ">
+            <div>
+              <h4 className="text-xl font-bold mb-5 mt-5 lg:mt-0 ">
+                Sign up to get offers
+              </h4>
+            </div>
+            <div className="  lg:mx-0 mx-12 md:mx-0">
+              <form className="flex border-slate-400 border-2 rounded-3xl  py-1 overflow-hidden     ">
+                <input   type="email"  placeholder="Email Address"   className="bg-transparent pl-4 outline-none w-full "  />
+                <button  type="submit"  className="bg-blue-800 hover:bg-blue-900 px-4 py-1 -my-1"   >
+                 Go
+                </button>
               </form>
             </div>
-            <div className=" flex  gap-2 mt-9">
+            <div className="flex gap-4 mt-9  justify-center  ">
               <div>
                 <a href="#">
-                  <img src={FooterImage2} alt="image" className="  h-10 w-auto" />
+                  <img src={FooterImage1} alt="image" className="h-10 w-auto " />
                 </a>
               </div>
               <div>
                 <a href="#">
-                  <img src={FooterImage1} alt="image" className="  h-10 w-auto" />
+                  <img src={FooterImage2} alt="image" className="h-10 w-auto" />
                 </a>
               </div>
             </div>
           </div>
         </div>
       </div>
+
       <div className=" w-full  min-h-0.5  bg-white  mt-5 mb-3"></div>
 
       <div className="flex w-full pt-1">
@@ -115,6 +125,8 @@ const Footer = () => {
           <p className=" pl-16">Shipping. © 2024. All Right Reserved.</p>
         </div>
       </div>
+
+      
     </footer>
   );
 };
